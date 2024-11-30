65ce32500752a3b7d12e1c6c0ef88a6e1b0fbe96
Sriram12052002 <sriram2182@gmail.com>
2024-11-30 22:12:50 +0530

    Update MavenTest.java

--- src/test/java/MavenTest.java
+++ src/test/java/MavenTest.java
@@ -12,7 +12,7 @@ WebDriver driver;
 		 driver = new ChromeDriver();
 		
 		driver.get("https://www.google.com");
-		System.out.println("chrome was opened");
+		System.out.println("chrome  opened");
 	}
 	
 	
@@ -20,6 +20,6 @@ WebDriver driver;
 	void closeChrome() throws InterruptedException {
 		Thread.sleep(3000);
 		driver.close();
-		System.out.println("chrome was closed");
+		System.out.println("chrome  closed");
 	}
 }
